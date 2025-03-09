5300c8fa0943e4ed0c83622aa52b2a69f820b975
Valerie Childers <vgchilders@wpi.edu>
2025-03-09 19:15:29 -0400

    user account err but admin runs

--- ATMapp/obj/Debug/net8.0/ATMapp.AssemblyInfo.cs
+++ ATMapp/obj/Debug/net8.0/ATMapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ATMapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a914371dc06a57c48eda24e4230da879db01e2ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+941aba8ca51c6f3265f91a697dcf83b377583cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ATMapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ATMapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
